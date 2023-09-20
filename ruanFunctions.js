58f4960a6ae384e2c7d430bcee70d052b7ec202d
Eduardo Alvarez <opticoedu@gmail.com>
2023-09-20 11:10:24 -0300

    ruanFunctions todos resolvidos

--- ruanFunctions.js
+++ ruanFunctions.js
@@ -84,25 +84,153 @@ Digite o primeiro número:
 Digite o segundo número:
 Qual operação deseja realizar? 1 – Soma 2 – Subtração 3 – Multiplicação 4 - Divisão*/
 
+/*Criar uma calculadora utilizando funções para cada operação. O usuário deve digitar o
+primeiro número, o segundo número e em seguida a operação que deseja realizar (1 -
+Soma 2 - Subtração 3 - Multiplicação 4 - Divisão). O resultado deve ser mostrado na tela.
+Digite o primeiro número:
+Digite o segundo número:
+Qual operação deseja realizar? 1 – Soma 2 – Subtração 3 – Multiplicação 4 - Divisão*/
+
+function soma(a, b) {
+    return a + b;
+}
+
+function resta(a, b) {
+    return a - b;
+}
+
+function multiplica(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+operacao = parseInt(prompt("Qual operação deseja realizar?\n1 – Soma\n2 – Subtração\n3 – Multiplicação\n4 - Divisão"));
 
 
+let a = parseFloat(prompt("Digite o primeiro número:"));
+let b = parseFloat(prompt("Digite o segundo número:"));
 
+let resultado;
+
+switch(operacao) {
+
+    case 1:
+    resultado = soma(a, b);
+    break;
+
+    case 2:
+    resultado = resta(a, b);
+    break;                      //////  ok
+
+    case 3:
+    resultado = multiplica(a, b);
+    break;
+
+    case 4:
+    resultado = divide(a, b);
+    break;
+
+    default:
+
+    alert('operacao nao permitida')
+
+}
+
+alert(`O resultado é: ${resultado}`);
 
 /*Crie 2 funções: a primeira recebe 3 números como argumento, realize a soma desses
 3 números e chama a segunda função, que por sua vez recebe como argumento uma
 soma e uma quantidade e retorna a média. Armazene o resultado da média em uma
 variável e exiba por um alert.*/
 
+function somar(a, b, c) {
+    return  a + b+ c;
+}                                        /// ok
 
+function media(soma, quantidade) {
+    return soma / quantidade;
+}
 
+let num1 = parseFloat(prompt('Digite o primer numero'));
+let num2 = parseFloat(prompt('Digite o segundo numero'));
+let num3 = parseFloat(prompt('Digite o tercer numero'));
+
+let somaNum = somar(num1, num2, num3);
+
+let quantidadeNum = 3;
+
+let mediaNum = media(somaNum, quantidadeNum);
+
+alert(`A media e ${mediaNum}`)
 
 
 /*Faça um programa que recebe três números do usuário, e identifica o maior através
 de uma função e o menor número através de outra função e depois os exibe por um
 alert.*/
 
+let primerNumero = parseFloat(prompt('Digite o primer numero'));
+let segundoNumero = parseFloat(prompt('Digite o segundo numero'));
+let tercerNumero = parseFloat(prompt('Digite o tercer numero'));
+
+function maior(a, b, c) {
+    if(a > b && a > c) {                              
+        return a
+    } else if(b > a && b > c) {
+        return b
+    } else {
+        return c
+    }
+}
+function menor(a, b, c) {
+    if(a < b && a < c) {                               /// ok
+        return a
+    } else if(b < a && b < c) {
+        return b
+    } else {
+        return c
+    }
+}
+
+let resultadoMaior = maior(primerNumero, segundoNumero, tercerNumero);
+let resultadoMenor = menor(primerNumero, segundoNumero, tercerNumero);
+
+alert(`O numero maior e ${resultadoMaior} e o numero menor e ${resultadoMenor}`)
 
 
 
 /*Crie uma função que recebe um número inteiro e retorna todos os primos, de 1 até
 ele.*/
+
+function encontrarPrimos(numeroDigitado) {
+    
+    let listaPrimos = [];
+
+   for(i = 2; i <= numeroDigitado; i++) {
+
+       var primo = true; 
+
+         for(x = 2; x < i; x++) {
+            if (i % x == 0) {
+            primo = false;
+            break;
+         }
+       }
+
+          if (primo) {
+            listaPrimos.push(i)
+          }
+   }
+    
+   return listaPrimos
+}
+
+
+var numeroDigitado = parseInt(prompt('Digite um numero inteiro'))
+
+var primosAteNumero = encontrarPrimos(numeroDigitado);
+
+
+alert(`Os numeros primos ate: ${numeroDigitado} sao:\n ${primosAteNumero}`)
